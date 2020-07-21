7e55ade504af9a5ae3fc3337a774b81afaa965e1
wendy03 <wendysu03@gmail.com>
2020-07-22 02:43:31 +0800

    update UI

--- js/products.js
+++ js/products.js
@@ -1,32 +1,24 @@
-const apiPath = 'https://course-ec-api.hexschool.io/api/';
+import Pagination from './pagination.js';
+import Navbar from './navbar.js';
+import Sidebar from './sidebar.js';
+import Spinner from './spinner.js';
+import zh_TW from './zh_TW.js';
 
-//分頁元件
-Vue.component('Pagination', {
-  template: '#pagination',
-  props: ['pages'],
-  methods: {
-    changePage(page) {
-      this.$emit('changePage', page);
-    },
+Vue.component('loading', VueLoading);
+
+// 將 VeeValidate input 驗證工具載入 作為全域註冊
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+// 將 VeeValidate 完整表單 驗證工具載入 作為全域註冊
+Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
+
+// 加入至 VeeValidate 的設定檔案
+VeeValidate.localize('tw', zh_TW);
+
+VeeValidate.configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
   },
-});
-
-Vue.component('Navbar', {
-  template: '#navbar',
-  methods: {
-    signout() {
-      document.cookie = `hexToken=; expires=; path=/`;
-      window.location = 'index.html';
-    },
-  },
-});
-
-Vue.component('Sidebar', {
-  template: '#sidebar',
-});
-
-Vue.component('Spinner', {
-  template: '#spinner',
 });
 
 new Vue({
@@ -36,6 +28,7 @@ new Vue({
       user: {
         token: '',
         uuid: '28c229ba-c1c4-47f6-9be0-210c99409aa6',
+        apiPath: 'https://course-ec-api.hexschool.io/api/',
       },
       products: [],
       tempProduct: {
@@ -49,11 +42,18 @@ new Vue({
       isLoading: true,
     };
   },
+  components: {
+    Pagination,
+    Navbar,
+    Sidebar,
+    Spinner,
+  },
   created() {
     this.user.token = document.cookie.replace(
       /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
       '$1'
     );
+    axios.defaults.headers['Authorization'] = `Bearer ${this.user.token}`;
     if (this.user.token === '') {
       window.location = 'index.html';
     } else {
@@ -63,8 +63,8 @@ new Vue({
   methods: {
     getProducts(page = 1) {
       const vm = this;
-      const url = `${apiPath}${vm.user.uuid}/admin/ec/products?page=${page}`;
-      axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
+      const url = `${vm.user.apiPath}${vm.user.uuid}/admin/ec/products?page=${page}`;
+      // axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
       axios
         .get(url)
         .then((res) => {
@@ -86,9 +86,7 @@ new Vue({
         $('#productModal').modal('show');
         this.isNew = true;
       } else if (type === 'edit') {
-        vm.tempProduct = Object.assign({}, item);
-        vm.getPorduct(vm.tempProduct.id);
-        $('#productModal').modal('show');
+        vm.getPorduct(item.id);
         this.isNew = false;
       } else if (type === 'del') {
         vm.tempProduct = Object.assign({}, item);
@@ -97,20 +95,21 @@ new Vue({
     },
     getPorduct(id) {
       const vm = this;
-      const url = `${apiPath}${vm.user.uuid}/admin/ec/product/${id}`;
-      axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
+      const url = `${vm.user.apiPath}${vm.user.uuid}/admin/ec/product/${id}`;
+      // axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
       axios.get(url).then((res) => {
         console.log(res);
         vm.tempProduct = res.data.data;
+        $('#productModal').modal('show');
       });
     },
     updateProduct() {
       const vm = this;
-      let url = `${apiPath}${this.user.uuid}/admin/ec/product`;
-      axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
+      let url = `${vm.user.apiPath}${vm.user.uuid}/admin/ec/product`;
+      // axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
       let httpMethod = 'post';
       if (!vm.isNew) {
-        url = `${apiPath}${this.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
+        url = `${vm.user.apiPath}${vm.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
         httpMethod = 'patch';
       }
 
@@ -147,8 +146,8 @@ new Vue({
       const formData = new FormData();
       formData.append('file', uploadedfile);
       vm.state.fileUpLoading = true;
-      const url = `${apiPath}${this.user.uuid}/admin/storage`;
-      axios.defaults.headers['Authorization'] = `Bearer ${this.user.token}`;
+      const url = `${vm.user.apiPath}${this.user.uuid}/admin/storage`;
+      // axios.defaults.headers['Authorization'] = `Bearer ${this.user.token}`;
       axios
         .post(url, formData, {
           headers: { 'Content-Type': 'multipart/form-data' },
@@ -157,6 +156,7 @@ new Vue({
           if (res.status === 200) {
             vm.state.fileUpLoading = false;
             this.tempProduct.imageUrl.push(res.data.data.path);
+            document.querySelector('#customFile').value = '';
           }
         })
         .catch((err) => {
@@ -172,8 +172,8 @@ new Vue({
     },
     delProduct() {
       const vm = this;
-      const url = `${apiPath}${this.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
-      axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
+      const url = `${vm.user.apiPath}${this.user.uuid}/admin/ec/product/${vm.tempProduct.id}`;
+      // axios.defaults.headers['Authorization'] = `Bearer ${vm.user.token}`;
       axios
         .delete(url)
         .then((res) => {
